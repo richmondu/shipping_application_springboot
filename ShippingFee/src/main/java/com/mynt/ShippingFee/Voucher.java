4ce2584817e20f203a969ddea5df9ad39820f88a
richmondu <richmond.umagat@yahoo.com>
2020-10-04 11:09:30 +0800

    Update Voucher.java

--- ShippingFee/src/main/java/com/mynt/ShippingFee/Voucher.java
+++ ShippingFee/src/main/java/com/mynt/ShippingFee/Voucher.java
@@ -6,11 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 
 
 public class Voucher {
     private final String host = "https://mynt-exam.mocklab.io";
     private String code;
+    private boolean expired;
 
     public Voucher(String code){
         this.code = code;
@@ -24,18 +28,28 @@ public class Voucher {
         if (response == null) {
             return 0;
         }
-        return parseDiscount(response);
+        try {
+            JsonNode jsonResponse = new ObjectMapper().readTree(response);
+            this.expired = checkExpiry(jsonResponse);
+            return parseDiscount(jsonResponse);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 
     public String getCode() {
         return this.code;
     }
 
+    public boolean isExpired() {
+        return this.expired;
+    }
+
     private String callVoucherApi(String code) {
         final String api = "/voucher/" + code + "?key=apikey";
-        final String baseUrl = host + api;
         try {
-            ResponseEntity<String> response = new RestTemplate().getForEntity(new URI(baseUrl), String.class);
+            ResponseEntity<String> response = new RestTemplate().getForEntity(new URI(host + api), String.class);
             return response.getBody();
         } catch (Exception e) {
             e.printStackTrace();
@@ -43,13 +57,26 @@ public class Voucher {
         }
     }
 
-    private float parseDiscount(String response) {
+    private float parseDiscount(JsonNode jsonResponse) {
+        return jsonResponse.get("discount").floatValue();
+    }
+
+    private String parseExpiry(JsonNode jsonResponse) {
+        return jsonResponse.get("expiry").asText();
+    }
+
+    private boolean checkExpiry(JsonNode jsonResponse) {
+        String dateStr = jsonResponse.get("expiry").asText();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         try {
-            JsonNode jsonNode = new ObjectMapper().readTree(response);
-            return jsonNode.get("discount").floatValue();
+            Date d1 = dateFormat.parse(dateStr);
+            Date d2 = dateFormat.parse(LocalDate.now().toString());
+            if (d1.compareTo(d2) < 0) {
+                return true;
+            }
         } catch (Exception e) {
             e.printStackTrace();
-            return 0;
         }
+        return false;
     }
 }
